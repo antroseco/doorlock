e59492dc101e6cbfb036ae426ae21194ee4a59fc
Antros Economides <antroseconomides@hotmail.com>
2018-04-30 17:17:17 +0300

    Make the REST API a singleton

--- api.js
+++ api.js
@@ -3,90 +3,88 @@
 const Router = require("koa-router");
 const Hardware = require("./hardware.js")
 
-class RestApi {
-    constructor() {
-        this.Controllers = {};
+const Rest = {
+    Controllers: {},
 
-        this.api = new Router({
-            prefix: "/api/v1"
-        });
+    api: new Router({
+        prefix: "/api/v1"
+    }),
 
-        this.api
-            // Stateless CSRF protection
-            .use(async (ctx, next) => {
-                if (ctx.headers.origin) {
-                    var Origin = ctx.headers.origin;
-                } else if (ctx.headers.referer) {
-                    const Referer = new URL(ctx.headers.referer);
+    get routes() {
+        return Rest.api.routes();
+    },
 
-                    var Origin = Referer.origin;
-                }
-
-                ctx.assert(Origin == "https://192.168.1.254"
-                || Origin == "https://raspberrypi.home"
-                || Origin == "https://raspberrypi", 403);
-
-                await next();
-            })
-            // Save client ID
-            .use(async (ctx, next) => {
-                ctx.state.id = ctx.socket.getPeerCertificate().subject.CN;
-
-                await next();
-            })
-            // Verify device exists
-            .param("device", async (device, ctx, next) => {
-                ctx.assert(this.Controllers[device], 404);
-
-                await next();
-            })
-            // Trigger device
-            .post("/:device", ctx => {
-                this.Controllers[ctx.params.device].Open(ctx.state.id);
-
-                ctx.status = 204;
-            })
-            // Query lock status
-            .get("/:device/lock", ctx => {
-                ctx.type = "application/json";
-                ctx.body = JSON.stringify({
-                    status: this.Controllers[ctx.params.device].Locked
-                });
-            })
-            // Set lock value
-            .post("/:device/lock", async ctx => {
-                try {
-                    const Post = await GetPost(ctx);
-                    const Data = JSON.parse(Post);
-
-                    ctx.assert(typeof Data.status == "boolean", 400);
-                    this.Controllers[ctx.params.device].Lock(ctx.state.id, Data.status);
-
-                    ctx.status = 204;
-                } catch(error) {
-                    /* GetPOST() throws 413 if the request is too large,
-                     * otherwise assume a malformed JSON payload */
-                    ctx.throw(error == 413 ? 413 : 400);
-                }
-            });
-    };
+    get allowedMethods() {
+        return Rest.api.allowedMethods();
+    },
 
     Register(Component) {
         if (Component instanceof Hardware.Controller) {
-            this.Controllers[Component.Name] = Component;
+            Rest.Controllers[Component.Name] = Component;
         } else {
             throw Error("Invalid arguments");
         }
-    };
-
-    get routes() {
-        return this.api.routes();
-    };
-
-    get allowedMethods() {
-        return this.api.allowedMethods();
-    };
+    }
 };
+
+Rest.api
+    // Stateless CSRF protection
+    .use(async (ctx, next) => {
+        if (ctx.headers.origin) {
+            var Origin = ctx.headers.origin;
+        } else if (ctx.headers.referer) {
+            const Referer = new URL(ctx.headers.referer);
+
+            var Origin = Referer.origin;
+        }
+
+        ctx.assert(Origin == "https://192.168.1.254"
+            || Origin == "https://raspberrypi.home"
+            || Origin == "https://raspberrypi", 403);
+
+        await next();
+    })
+    // Save client ID
+    .use(async (ctx, next) => {
+        ctx.state.id = ctx.socket.getPeerCertificate().subject.CN;
+
+        await next();
+    })
+    // Verify device exists
+    .param("device", async (device, ctx, next) => {
+        ctx.assert(Rest.Controllers[device], 404);
+
+        await next();
+    })
+    // Trigger device
+    .post("/:device", ctx => {
+        Rest.Controllers[ctx.params.device].Open(ctx.state.id);
+
+        ctx.status = 204;
+    })
+    // Query lock status
+    .get("/:device/lock", ctx => {
+        ctx.type = "application/json";
+        ctx.body = JSON.stringify({
+            status: Rest.Controllers[ctx.params.device].Locked
+        });
+    })
+    // Set lock value
+    .post("/:device/lock", async ctx => {
+        try {
+            const Post = await GetPost(ctx);
+            const Data = JSON.parse(Post);
+
+            ctx.assert(typeof Data.status == "boolean", 400);
+            Rest.Controllers[ctx.params.device].Lock(ctx.state.id, Data.status);
+
+            ctx.status = 204;
+        } catch (error) {
+            /* GetPOST() throws 413 if the request is too large,
+             * otherwise assume a malformed JSON payload */
+            ctx.throw(error == 413 ? 413 : 400);
+        }
+    });
 
 function GetPost(ctx) {
     let Buffer = "";
@@ -108,4 +106,4 @@ function GetPost(ctx) {
     });
 };
 
-module.exports = RestApi;
+module.exports = Rest;
